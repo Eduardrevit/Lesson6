ed6c322eb7316a62a152cad499b7db996f336e60
Эдуард <@LAPTOP-2C6APU5G>
2022-11-29 22:30:19 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string str2 = "";
+            
+            foreach (char c in str)
+            {
+                str2 = c + str2; 
+            }
+           
+            if (str==str2)
+            {
+                Console.WriteLine("Строка является палиндромом");
+            }
+            else
+            {
+                Console.WriteLine("Строка НЕ является палиндромом");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
